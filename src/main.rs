5f362a3284f8ac9f44231eaee8f571dfb8f0273d
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2023-03-26 15:58:38 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use ev3dev_lang_rust::Ev3Result;
 use ev3dev_lang_rust::motors::{TachoMotor, MotorPort};
 use ev3dev_lang_rust::sensors::UltrasonicSensor;
 
+const FOV: f32 = 70f32;
+
+const speed: i32 = 100i32;
+
+
 fn main() -> Ev3Result<()> {
     let ultrasonicsensor = UltrasonicSensor::find()?;
 
@@ -13,14 +18,14 @@ fn main() -> Ev3Result<()> {
 
     radar_motor.run_direct()?;
 
-    radar_motor.set_duty_cycle_sp(50)?;
+    radar_motor.set_duty_cycle_sp(speed)?;
     while true {
         let position: f32 = radar_motor.get_position()? as f32;
         
-        if position >= 70f32 {
-            radar_motor.set_duty_cycle_sp(-50)?;
-        } else if position <= -70f32 {
-            radar_motor.set_duty_cycle_sp(50)?;
+        if position >= FOV {
+            radar_motor.set_duty_cycle_sp(-speed)?;
+        } else if position <= -FOV {
+            radar_motor.set_duty_cycle_sp(speed)?;
         }
 
         println!("distance: {}, motor position: {}", ultrasonicsensor.get_distance_centimeters().unwrap(), position);
