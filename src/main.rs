00cf149a57d0f744797fd34596f9f233e40af50b
neovim-hardcore-user <113374980+neovim-hardcore-user@users.noreply.github.com>
2023-03-26 15:53:47 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 extern crate ev3dev_lang_rust;
 
 use ev3dev_lang_rust::Ev3Result;
-use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
+use ev3dev_lang_rust::motors::{TachoMotor, MotorPort};
 use ev3dev_lang_rust::sensors::UltrasonicSensor;
 
 fn main() -> Ev3Result<()> {
     let ultrasonicsensor = UltrasonicSensor::find()?;
 
-    let radar_motor = LargeMotor::get(MotorPort::OutA)?;
+    let radar_motor = TachoMotor::get(MotorPort::OutD)?;
+    
+    radar_motor.reset();
+
     radar_motor.run_direct()?;
 
     radar_motor.set_duty_cycle_sp(50)?;
-
     while true {
         let position: f32 = radar_motor.get_position()? as f32;
         
-        if position <= 180f32 {
+        if position >= 70f32 {
             radar_motor.set_duty_cycle_sp(-50)?;
-        } else if position >= -180f32 {
+        } else if position <= -70f32 {
             radar_motor.set_duty_cycle_sp(50)?;
         }
 
