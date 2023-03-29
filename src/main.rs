5fa3d2f63c31657e2e475dc52f5b4bb739ac3945
NicoDevStuff <NicoG2008@gmx.de>
2023-03-29 21:09:57 +0200

    woieuhf'oowjlf;kjwe
    yes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
-extern crate ev3dev_lang_rust;
+use console_engine::pixel;
+use console_engine::Color;
+use console_engine::KeyCode;
 
-use ev3dev_lang_rust::Ev3Result;
-use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
-use ev3dev_lang_rust::sensors::ColorSensor;
+fn main() {
+    // initializes a screen of 20x10 characters with a target of 3 frames per second
+    // coordinates will range from [0,0] to [19,9]
+    let mut engine = console_engine::ConsoleEngine::init(20, 10, 3).unwrap();
+    let value = 14;
+    // main loop, be aware that you'll have to break it because ctrl+C is captured
+    loop {
+        engine.wait_frame(); // wait for next frame + capture inputs
+        engine.clear_screen(); // reset the screen
+    
+        engine.line(0, 0, 19, 9, pixel::pxl('#')); // draw a line of '#' from [0,0] to [19,9]
+        engine.print(0, 4, format!("Result: {}", value).as_str()); // prints some value at [0,4]
+    
+        engine.set_pxl(4, 0, pixel::pxl_fg('O', Color::Cyan)); // write a majestic cyan 'O' at [4,0]
 
-fn main() -> Ev3Result<()> {
-
-    // Get large motor on port outA.
-    let large_motor = LargeMotor::get(MotorPort::OutA)?;
-
-    // Set command "run-direct".
-    large_motor.run_direct()?;
-
-    // Run motor.
-    large_motor.set_duty_cycle_sp(50)?;
-
-    // Find color sensor. Always returns the first recognized one.
-    let color_sensor = ColorSensor::find()?;
-
-    // Switch to rgb mode.
-    color_sensor.set_mode_rgb_raw()?;
-
-    // Get current rgb color tuple.
-    println!("Current rgb color: {:?}", color_sensor.get_rgb()?);
-
-    Ok(())
+        if engine.is_key_pressed(KeyCode::Char('q')) { // if the user presses 'q' :
+            break; // exits app
+        }
+    
+        engine.draw(); // draw the screen
+    }
 }
